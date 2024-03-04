54e2ed92c98f2c5b1af2e42c4c43448d345049af
Roland Welter <77165414+rkwelter@users.noreply.github.com>
2024-03-04 11:24:05 +0100

    Updated saving method in HeatTransport_Iterator.m

--- HeatTransport/HeatTransport_Iterator.m
+++ HeatTransport/HeatTransport_Iterator.m
@@ -11,18 +11,20 @@ addpath('../FluidSolver'); addpath('../FluidSolver/TimeSteppers/');
 
 % select parameters
 
-M=105;
+M=190;
 Pr = 10;
 k1 = 1/sqrt(2); V = pi/sqrt(2*k1);
+RaVec = 7500;
 %RaVec = [1 (1:1:4)*10^3/4 (1:1:4)*10^4/4 (1:1:4)*10^5/4 (3:1:20)*10^5/2 ]; % Large, course Rayleigh grid
 %RaVec = [1 (1:1:10)*50 (6:1:10)*10^2 (2:1:5)*10^3 ]; % small, fine Rayleigh grid
-RaVec = 5; %[1 , 5:5:2000]; % hyperfine Rayleigh grid
+%RaVec = [1 , 5:5:2000]; % hyperfine Rayleigh grid
 %RoVec = (0:1:10)*10^2; % Large, course rotation grid
 %RoVec = 0:50:300; % Small, fine rotation grid
 RoVec = 0; % no rotation
+errTol = 10^(-7);
 
 numRuns = 1; % numRuns controls how many different trajectories are computed (ie different init. cond.) for each Rayleigh/rotation number
-extendLimit = 100; % extendLimit sets a maximum number of iterations that the solver continues a trajectory if the variation in the heat transport remains large
+extendLimit = 20; % extendLimit sets a maximum number of iterations that the solver continues a trajectory if the variation in the heat transport remains large
 
 % generate variables
 
@@ -49,7 +51,6 @@ end
 
 % Integrate
 
-options = odeset('RelTol',1e-12,'AbsTol',1e-12*ones(1,n));
 extensions = 0;
 progress = 0;
 totaljobs = length(RaVec)*length(RoVec)*numRuns;
@@ -65,29 +66,28 @@ for i=1:length(RaVec)
             if(isfile(name))
                 allData = load(name);
                 X = allData.X;
-                tInc = allData.tInc;
-                Tf = allData.Tf;
-                runTime = allData.runTime;
+                parameters = allData.parameters;
                 heatTransport = allData.heatTransport;
                 limitStdDevPercent = allData.limitStdDevPercent;
-                addTime = 10^3*tInc;
+                addTime = 500*tInc;
             else
-                Up0 = 0.001*randn(numUp,1);
-                Um0 = 0.001*randn(numUm,1);
-                The0 = The0nearUni + 0.001*randn(numTheta,1);
+                
+                Up0 = 0.05*randn(numUp,1)./(uPlusModes(:,1).^2+uPlusModes(:,2).^2);
+                Um0 = 0.05*randn(numUm,1)./(uMinusModes(:,1).^2+uMinusModes(:,2).^2);
+                The0 = The0nearUni + 0.05*randn(numTheta,1)./(thetaModes(:,1).^2+thetaModes(:,2).^2);
                 
                 X=[Up0;Um0;The0]';
-                tInc=min(0.0001,10/(RaVec(i)));
+                tInc=10^(-5); %min(0.0001,10/(RaVec(i)));
                 Tf = 0;
                 runTime = 0;
                 heatTransport = [];
                 limitStdDevPercent = 1;
-                addTime = 10^4*tInc;
+                addTime = 500*tInc;
+                parameters = [Pr Ro k1 Ra tInc Tf M runTime errTol];
             end 
 
-            parameters = [Pr Ro k1 Ra tInc Tf M runTime];
 
-            while( limitStdDevPercent > .01 && extensions < extendLimit ) % && size(X,1) < 5*10^4 )
+            while( extensions < extendLimit ) % limitStdDevPercent > .01 &&
                 %addTime = 10^4*(extendLimit-extensions)*tInc;
                 [t,X,parameters] = FluidSolver(parameters,X,addTime,0);
 
@@ -103,13 +103,12 @@ for i=1:length(RaVec)
                 heatTransport = 1+tInc*cumsum(NusseltIntegrand)./t;
                 limitStdDevPercent = std(heatTransport(floor(end/2):end))/heatTransport(end);
                 extensions = extensions + 1
-                Tf = Tf + addTime;
-                addTime = 10^3*tInc;
+                %Tf = Tf + addTime;
+                addTime = 500*tInc;
             end
                             
             if(extensions > 0)
-                Pr = parameters(1); Ro = parameters(2); k1 = parameters(3); Ra = parameters(4); tInc=parameters(5); Tf = parameters(6); M = parameters(7); runTime = parameters(8);
-                save(name,'Ra','Pr','k1','Ro','Tf','tInc','M','runTime','X','heatTransport','limitStdDevPercent');
+                save(name,'parameters','X','heatTransport','limitStdDevPercent');
             end
             extensions = 0;
         end
